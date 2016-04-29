da374b72e637112ad075d5aeb56e5869bb622d6e
nikkolasg <nikkolasg@gmail.com>
2016-04-29 21:54:56 +0200

    local processor options

--- tufsim.rb
+++ tufsim.rb
@@ -11,18 +11,21 @@ require_relative 'processor'
 DEFAULT_BASE = 2
 DEFAULT_HEIGHT = 2
 
-@options = {:base => DEFAULT_BASE, :height => DEFAULT_HEIGHT}
+@options = {:base => DEFAULT_BASE, :height => DEFAULT_HEIGHT, :type => :local}
 
 OptionParser.new do |opts|
     opts.banner = "Usage tufsim.rb <processor> [options]"
     opts.on("-b","--base BASE",'Base of the skiplist') do |b|
         @options[:base] = b.to_i
     end
-    opts.on("-l","--heigh HEIGHT","Maximum height of the skiplist") do |h|
+    opts.on("-i","--heigh HEIGHT","Maximum height of the skiplist") do |h|
         @options[:height] = h.to_i
     end
     opts.on("-n","--head NUMBER","Takes only the first NUMBER snapshots") do |n|
         @options[:head] = n.to_i
+    end
+    opts.on("-t","--type TYPE","Between SSH and LOCAL") do |t|
+        @options[:type] = t.downcase.to_sym
     end
     opts.on('-h', '--help', 'Displays Help') do
         puts opts
@@ -35,34 +38,47 @@ def args
     @options[:processor]= ARGV.shift
 end
 
-def main 
-    puts "[+] Tufsim.rb <#{@options[:processor]}> with base = #{@options[:base]} & height = #{@options[:height]}"
-    # instantiate mockup class
-    mockup = Mockup::SSH.new
-    mockup.connect
-    ## first get the list of snapshots
-    snaps = mockup.snapshots 
-    ## then the packages
-    mockup.packages_size
-    ## construct the skiplist out of it
-    skiplist = Skipchain::create_skiplist snaps, Skipchain::Config.new(@options[:base],@options[:height],@options[:head])
-    ## fetch and map the client updates
-    updates = mockup.client_updates skiplist.mapping_client_update()
-
-    ## verification
-    #tssSkip = skiplist.timestamps
-    #updates.each do |k,v| 
-    #    puts "WOW" if v.find_all { |ts| !tssSkip.key?(ts)}.size > 0
-    #end
-
-    ## run the processor
-    processor = nil
-    case @options[:processor].downcase
-    when "onelevel"
-        processor = Processor::OneLevel.new mockup,updates,skiplist
+def new_mockup
+    case @options[:type] 
+    when :local
+        m = Mockup::Local.new
+        mockup.packages_size
+        yield m
+    when :ssh
+        m = Mockuo::SSH.new
+        m.connect
+        mockup.packages_size
+        yield m
+        m.close
+    else
+        abort("[-] Unknown mockup class")
     end
-    result = processor.process
-    mockup.close
+end
+
+def main 
+    puts "[+] Tufsim.rb (#{@options[:type]})<#{@options[:processor]}> with base = #{@options[:base]} & height = #{@options[:height]}"
+    new_mockup do |mockup| 
+        ## first get the list of snapshots
+        snaps = mockup.snapshots 
+        ## construct the skiplist out of it
+        skiplist = Skipchain::create_skiplist snaps, Skipchain::Config.new(@options[:base],@options[:height],@options[:head])
+        ## fetch and map the client updates
+        updates = mockup.client_updates skiplist.mapping_client_update()
+
+        ## verification
+        #tssSkip = skiplist.timestamps
+        #updates.each do |k,v| 
+        #    puts "WOW" if v.find_all { |ts| !tssSkip.key?(ts)}.size > 0
+        #end
+
+        ## run the processor
+        processor = nil
+        case @options[:processor].downcase
+        when "onelevel"
+            processor = Processor::OneLevel.new mockup,updates,skiplist
+        end
+        result = processor.process
+    end
 end
 
 args
